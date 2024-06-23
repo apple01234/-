36ba3d13fbdac0185a83ab748d954709006f218c
최승현 <111742198+apple01234@users.noreply.github.com>
2024-06-23 20:18:56 +0900

    Add files via upload

---
+++ 룰루 바이러스/index.js
@@ -0,0 +1,8 @@
+setInterval(()=>{
+
+let imgs = document.querySelectorAll('img');
+imgs.forEach((a, i) => {
+    a.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfk6WCW3FgITRIl7gZ3W9ly4XIdZYE8ZpocQ&s'
+})
+
+}, 500)
